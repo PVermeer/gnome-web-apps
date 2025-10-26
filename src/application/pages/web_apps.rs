71ca811fa73f3c9882000d8c1b5a23de761bead4
Pim Vermeer <pim.vermeer@gmail.com>
2025-10-26 13:44:57 +0000

    feat(ui): added app section

--- src/application/pages/web_apps.rs
+++ src/application/pages/web_apps.rs
@@ -1,5 +1,13 @@
 use super::NavPage;
-use libadwaita::NavigationPage;
+use libadwaita::{
+    ActionRow, ButtonContent, NavigationPage, PreferencesGroup,
+    gtk::{
+        Button, Image,
+        prelude::{BoxExt, ButtonExt},
+    },
+    prelude::{ActionRowExt, PreferencesGroupExt},
+};
+use log::debug;
 
 pub struct WebAppsPage {
     pub nav_page: NavigationPage,
@@ -23,12 +31,50 @@ impl WebAppsPage {
     pub fn new() -> Self {
         let title = String::from("Web Apps");
         let icon = "preferences-desktop-apps-symbolic".to_string();
-        let (nav_page, _header, _content_box) = Self::build_nav_page(&title);
+        let (nav_page, _header, content_box) = Self::build_nav_page(&title);
+        let app_section = Self::build_apps_section();
+
+        content_box.append(&app_section);
 
         Self {
             nav_page,
             title,
             icon,
         }
+    }
+
+    fn build_apps_section() -> PreferencesGroup {
+        let button_content = ButtonContent::builder()
+            .label("New app")
+            .icon_name("list-add-symbolic")
+            .build();
+        let new_app_button = Button::builder()
+            .css_classes(["flat"])
+            .child(&button_content)
+            .build();
+        new_app_button.connect_clicked(|_| debug!("TODO"));
+
+        let pref_group = PreferencesGroup::builder()
+            .title("Apps")
+            .header_suffix(&new_app_button)
+            .build();
+        let web_app_row = Self::build_app_row("My web app");
+        pref_group.add(&web_app_row);
+
+        pref_group
+    }
+
+    fn build_app_row(title: &str) -> ActionRow {
+        let row = ActionRow::builder().title(title).activatable(true).build();
+        let prefix = Image::from_icon_name("web-browser-symbolic");
+        let suffix = Image::from_icon_name("go-next-symbolic");
+        row.add_prefix(&prefix);
+        row.add_suffix(&suffix);
+
+        row.connect_activated(|_| {
+            debug!("TODO");
+        });
+
+        row
     }
 }
