fe2edb5975d514da663d754016c846d49dc45c04
Pim Vermeer <pim.vermeer@gmail.com>
2025-10-28 13:18:30 +0000

    fix(app): removed weak reference for self declaration

--- src/application.rs
+++ src/application.rs
@@ -2,42 +2,31 @@ mod pages;
 mod window;
 
 use pages::{Page, Pages};
-use std::rc::{Rc, Weak};
+use std::rc::Rc;
 use window::AppWindow;
 
 pub struct App {
-    self_weak: Weak<App>,
     window: AppWindow,
     pages: Pages,
 }
 impl App {
     pub fn new(adw_application: &libadwaita::Application) -> Rc<Self> {
-        Rc::new_cyclic(|self_weak| {
+        Rc::new({
             let pages = Pages::new();
             let window = AppWindow::new(adw_application);
 
-            Self {
-                self_weak: self_weak.to_owned(),
-                window,
-                pages,
-            }
+            Self { window, pages }
         })
     }
 
-    pub fn init(&self) {
-        let app = &self.get_app();
-        app.window.init(app);
-        app.pages.init(app);
+    pub fn init(self: &Rc<Self>) {
+        self.window.init(self);
+        self.pages.init(self);
 
         self.navigate(&Page::Home);
     }
 
-    pub fn get_app(&self) -> Rc<App> {
-        self.self_weak.upgrade().unwrap()
-    }
-
-    pub fn navigate(&self, page: &Page) {
-        let app = &self.get_app();
-        self.window.view.navigate(app, page);
+    pub fn navigate(self: &Rc<Self>, page: &Page) {
+        self.window.view.navigate(self, page);
     }
 }
