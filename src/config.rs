028864bd3b6a008307270ac71eae4c3d02142bc7
Pim Vermeer <pim.vermeer@gmail.com>
2025-11-03 09:48:25 +0000

    chore(debug): disable debug logs for external crates

--- src/config.rs
+++ src/config.rs
@@ -4,6 +4,7 @@ pub const APP_ID: &str = "org.pvermeer.gnome-web-apps";
 pub const VERSION: &str = "0.0.0";
 pub const APP_NAME: &str = "Gnome Web Apps";
 pub const APP_NAME_PATH: &str = "gnome-web-apps";
+pub const APP_NAME_CRATE: &str = "gnome_web_apps";
 pub const DEVELOPER: &str = "PVermeer";
 pub const CREDITS: &[&str] = &["Some credits"];
 pub const ACKNOWLEDGEMENT: &[&str] = &["Some acknowledgement"];
--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,17 @@ mod config;
 use application::App;
 use env_logger::Env;
 use libadwaita::gio::prelude::{ApplicationExt, ApplicationExtManual};
+use log::LevelFilter;
 
 fn main() {
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    let mut logger = env_logger::Builder::from_env(Env::default().default_filter_or("info"));
+    if cfg!(debug_assertions) {
+        // Only enable debug logging for this app
+        logger
+            .filter_level(LevelFilter::Info)
+            .filter_module(config::APP_NAME_CRATE, LevelFilter::max());
+    }
+    logger.init();
 
     let adw_application = libadwaita::Application::builder()
         .application_id(config::APP_ID)
