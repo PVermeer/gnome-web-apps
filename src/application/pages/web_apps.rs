898d1ecddd88a3f9732af80f16643bb544bb08cc
Pim Vermeer <pim.vermeer@gmail.com>
2025-11-15 20:32:13 +0000

    feat(web-apps): implemented fs saving

--- src/application/pages/web_apps.rs
+++ src/application/pages/web_apps.rs
@@ -3,10 +3,13 @@ mod web_app_view;
 use super::NavPage;
 use super::PrefNavPage;
 use crate::application::App;
+use crate::application::browser_configs::Browser;
 use crate::application::pages::web_apps::web_app_view::WebAppView;
 use crate::config;
+use anyhow::Context;
+use anyhow::Result;
+use anyhow::bail;
 use freedesktop_desktop_entry::DesktopEntry;
-use freedesktop_desktop_entry::get_languages_from_env;
 use libadwaita::StatusPage;
 use libadwaita::{
     ActionRow, ButtonContent, NavigationPage, NavigationView, PreferencesGroup, PreferencesPage,
@@ -15,17 +18,32 @@ use libadwaita::{
 };
 use log::debug;
 use log::error;
+use rand::Rng;
+use rand::distributions::Alphanumeric;
+use regex::Regex;
 use std::borrow::Cow;
 use std::cell::RefCell;
+use std::fs;
 use std::path::Path;
+use std::path::PathBuf;
 use std::rc::Rc;
+use url::Url;
+
+struct DesktopFileEntries {
+    name: String,
+    id: String,
+    browser_id: String,
+    url: String,
+    domain: String,
+    isolate: bool,
+    icon: String,
+}
 
 pub struct WebAppsPage {
     nav_page: NavigationPage,
     nav_row: ActionRow,
     nav_view: NavigationView,
     prefs_page: PreferencesPage,
-    locales: Vec<String>,
 }
 impl NavPage for WebAppsPage {
     fn get_navpage(&self) -> &NavigationPage {
@@ -49,14 +67,11 @@ impl WebAppsPage {
             ..
         } = Self::build_nav_page(title, icon).with_preference_navigation_view();
 
-        let locales = get_languages_from_env();
-
         Rc::new(Self {
             nav_page,
             nav_row,
             nav_view,
             prefs_page,
-            locales,
         })
     }
 
@@ -80,7 +95,7 @@ impl WebAppsPage {
             .header_suffix(&new_app_button)
             .build();
 
-        let web_app_desktop_files = self.get_owned_desktop_files(app);
+        let web_app_desktop_files = Self::get_owned_desktop_files(app);
         if web_app_desktop_files.is_empty() {
             let status_page = StatusPage::builder()
                 .title("No Web Apps found")
@@ -107,7 +122,7 @@ impl WebAppsPage {
         let desktop_file_borrow = desktop_file.borrow();
 
         let app_name = desktop_file_borrow
-            .name(&self.locales)
+            .name(&app.desktop_file_locales)
             .unwrap_or(Cow::Borrowed("No name"));
         let app_row = ActionRow::builder()
             .title(app_name)
@@ -124,7 +139,7 @@ impl WebAppsPage {
         let app_clone = app.clone();
 
         app_row.connect_activated(move |_| {
-            let app_page = WebAppView::new(&app_clone, &desktop_file.clone(), &self.locales);
+            let app_page = WebAppView::new(&app_clone, &desktop_file.clone());
             app_page.init();
             self.nav_view.push(app_page.get_navpage());
         });
@@ -132,40 +147,45 @@ impl WebAppsPage {
         app_row
     }
 
-    fn get_owned_desktop_files(self: &Rc<Self>, app: &Rc<App>) -> Vec<Rc<RefCell<DesktopEntry>>> {
+    fn get_applications_path(app: &Rc<App>) -> Result<PathBuf> {
+        if cfg!(debug_assertions) {
+            let path = Path::new("./dev-assets/desktop-files").to_path_buf();
+            debug!("Using dev applications path: {}", path.display());
+            return Ok(path);
+        }
+
+        let Some(data_home_path) = app.dirs.data_home.as_ref() else {
+            bail!("Could not get data home path")
+        };
+
+        let path = data_home_path.join("applications");
+        debug!("Using applications path: {}", path.display());
+
+        if !path.is_dir() {
+            bail!("Could not get applications path");
+        }
+
+        Ok(path)
+    }
+
+    fn get_owned_desktop_files(app: &Rc<App>) -> Vec<Rc<RefCell<DesktopEntry>>> {
         debug!("Reading user desktop files");
 
         let owned_web_app_key = config::DesktopFile::GWA_KEY;
         let mut owned_desktop_files = Vec::new();
 
-        if cfg!(debug_assertions) {
-            debug!("Adding dev desktop files");
-
-            let dev_desktop_files_path = Path::new("./dev-assets/desktop-files");
-
-            for file in dev_desktop_files_path.read_dir().unwrap().flatten() {
-                if let Ok(desktop_file) = DesktopEntry::from_path(file.path(), Some(&self.locales))
-                {
-                    owned_desktop_files.push(Rc::new(RefCell::new(desktop_file)));
-                }
+        let applications_path = match Self::get_applications_path(app) {
+            Err(error) => {
+                error!("{error}");
+                return owned_desktop_files;
             }
-        }
-
-        let Some(data_home_path) = app.dirs.data_home.as_ref() else {
-            error!("Could not get data home path");
-            return owned_desktop_files;
+            Ok(path) => path,
         };
 
-        let applications_path = data_home_path.join("applications");
-        debug!("Using path: {}", applications_path.display());
-
-        if !applications_path.is_dir() {
-            error!("Could not get applications path");
-            return owned_desktop_files;
-        }
-
         for file in applications_path.read_dir().unwrap().flatten() {
-            let Ok(desktop_file) = DesktopEntry::from_path(file.path(), Some(&self.locales)) else {
+            let Ok(desktop_file) =
+                DesktopEntry::from_path(file.path(), Some(&app.desktop_file_locales))
+            else {
                 continue;
             };
 
@@ -192,5 +212,170 @@ impl WebAppsPage {
         } else {
             Image::from_icon_name(icon_name)
         }
+    }
+
+    fn get_desktop_file_entries(
+        app: &Rc<App>,
+        desktop_file: &DesktopEntry,
+    ) -> Result<DesktopFileEntries> {
+        match (|| -> Result<DesktopFileEntries> {
+            let name = desktop_file
+                .name(&app.desktop_file_locales)
+                .context("Missing 'Name'")?
+                .to_string();
+
+            let id = desktop_file
+                .desktop_entry(config::DesktopFile::ID_KEY)
+                .map(std::string::ToString::to_string)
+                .or_else(|| {
+                    let random_id: String = rand::thread_rng()
+                        .sample_iter(&Alphanumeric)
+                        .take(8)
+                        .map(char::from)
+                        .collect();
+                    Some(random_id)
+                })
+                .context(format!("Missing '{}'", config::DesktopFile::ID_KEY))?;
+
+            let browser_id = desktop_file
+                .desktop_entry(config::DesktopFile::BROWSER_ID_KEY)
+                .context(format!("Missing '{}'", config::DesktopFile::BROWSER_ID_KEY))?
+                .to_string();
+
+            let url = desktop_file
+                .desktop_entry(config::DesktopFile::URL_KEY)
+                .context(format!("Missing '{}'", config::DesktopFile::URL_KEY))?
+                .to_string();
+
+            let domain = Url::parse(&url)?
+                .domain()
+                .context("Failed to get domain of url")?
+                .to_string();
+
+            let isolate = desktop_file
+                .desktop_entry(config::DesktopFile::ISOLATE_KEY)
+                .context(format!("Missing '{}'", config::DesktopFile::ISOLATE_KEY))?
+                .eq("true");
+
+            let icon = desktop_file.icon().context("Missing 'Icon'")?.to_string();
+
+            Ok(DesktopFileEntries {
+                name,
+                id,
+                browser_id,
+                url,
+                domain,
+                isolate,
+                icon,
+            })
+        })() {
+            Ok(result) => Ok(result),
+            Err(error) => {
+                bail!("Failed to get all entries on desktop file: '{error}'")
+            }
+        }
+    }
+
+    fn get_desktop_file_save_path(
+        app: &Rc<App>,
+        desktop_files_entries: &DesktopFileEntries,
+        browser: &Browser,
+    ) -> Result<PathBuf> {
+        let applications_dir = Self::get_applications_path(app)?;
+        let file_name = format!(
+            "{}-{}{}",
+            browser.file_name,
+            config::APP_NAME_SHORT,
+            desktop_files_entries.id
+        );
+        let mut desktop_file_path = applications_dir.join(file_name);
+        desktop_file_path.add_extension("desktop");
+
+        Ok(desktop_file_path)
+    }
+
+    fn get_profile_isolation_path(app: &Rc<App>, app_id: &str) -> Result<PathBuf> {
+        let profiles_path = app.dirs.create_data_directory("profiles")?;
+        let path = profiles_path.join(app_id);
+        Ok(path)
+    }
+
+    fn save(app: &Rc<App>, desktop_file: &RefCell<DesktopEntry>) -> Result<()> {
+        if let Err(error) = (|| -> Result<()> {
+            let mut desktop_file = desktop_file.borrow_mut();
+            let entries = Self::get_desktop_file_entries(app, &desktop_file)?;
+            let browser = app
+                .browsers_configs
+                .get_by_id(&entries.browser_id)
+                .context("Failed to get browser")?;
+
+            let mut d_str = browser.desktop_file.clone().to_string();
+
+            d_str = d_str.replace(
+                config::DesktopFile::COMMAND_REPLACE,
+                &browser.get_command()?,
+            );
+            d_str = d_str.replace(config::DesktopFile::NAME_REPLACE, &entries.name);
+            d_str = d_str.replace(config::DesktopFile::URL_REPLACE, &entries.url);
+            d_str = d_str.replace(config::DesktopFile::DOMAIN_REPLACE, &entries.domain);
+            d_str = d_str.replace(config::DesktopFile::ICON_REPLACE, &entries.icon);
+
+            let isolate_key = "is_isolated";
+            let optional_isolated_value =
+                Regex::new(&format!(r"%\{{{isolate_key}\s*\?\s*([^}}]+)"))
+                    .unwrap()
+                    .captures(&d_str)
+                    .and_then(|caps| caps.get(1).map(|value| value.as_str().to_string()));
+
+            if let Some(value) = optional_isolated_value {
+                let path = Self::get_profile_isolation_path(app, &entries.id)?;
+                let re = Regex::new(&format!(r"%\{{{isolate_key}\s*\?\s*[^}}]+\}}",)).unwrap();
+
+                let replacement = if entries.isolate {
+                    format!("{value}={}", path.to_string_lossy())
+                } else {
+                    String::new()
+                };
+
+                d_str = re.replace_all(&d_str, replacement).to_string();
+            }
+
+            let save_path = Self::get_desktop_file_save_path(app, &entries, &browser)?;
+            let mut new_desktop_file =
+                DesktopEntry::from_str(&save_path, &d_str, Some(&app.desktop_file_locales))?;
+
+            new_desktop_file
+                .add_desktop_entry(config::DesktopFile::GWA_KEY.to_string(), "true".to_string());
+            new_desktop_file
+                .add_desktop_entry(config::DesktopFile::URL_KEY.to_string(), entries.url);
+            new_desktop_file.add_desktop_entry(config::DesktopFile::ID_KEY.to_string(), entries.id);
+            new_desktop_file.add_desktop_entry(
+                config::DesktopFile::BROWSER_ID_KEY.to_string(),
+                entries.browser_id,
+            );
+            new_desktop_file.add_desktop_entry(
+                config::DesktopFile::ISOLATE_KEY.to_string(),
+                entries.isolate.to_string(),
+            );
+
+            if desktop_file.path.is_file() {
+                match fs::remove_file(&desktop_file.path) {
+                    Ok(()) => {}
+                    Err(error) => {
+                        error!("Failed to remove desktop file before saving new: {error}");
+                    }
+                }
+            }
+
+            debug!("Saving desktop file to: {}", save_path.display());
+            fs::write(&save_path, new_desktop_file.to_string())?;
+            *desktop_file = new_desktop_file;
+
+            Ok(())
+        })() {
+            error!("{error}");
+            bail!(error)
+        }
+        Ok(())
     }
 }
--- src/config.rs
+++ src/config.rs
@@ -5,6 +5,7 @@ pub const VERSION: &str = "0.0.0";
 pub const APP_NAME: &str = "Gnome Web Apps";
 pub const APP_NAME_PATH: &str = "gnome-web-apps";
 pub const APP_NAME_CRATE: &str = "gnome_web_apps";
+pub const APP_NAME_SHORT: &str = "gwa";
 pub const DEVELOPER: &str = "PVermeer";
 pub const CREDITS: &[&str] = &["Some credits"];
 pub const ACKNOWLEDGEMENT: &[&str] = &["Some acknowledgement"];
@@ -15,6 +16,12 @@ impl DesktopFile {
     pub const GWA_KEY: &str = "X-GWA";
     pub const URL_KEY: &str = "X-GWA-URL";
     pub const ID_KEY: &str = "X-GWA-ID";
-    pub const BROWSER_ID: &str = "X-GWA-BROWSER-ID";
+    pub const BROWSER_ID_KEY: &str = "X-GWA-BROWSER-ID";
     pub const ISOLATE_KEY: &str = "X-GWA-ISOLATE";
+
+    pub const NAME_REPLACE: &str = "%{name}";
+    pub const COMMAND_REPLACE: &str = "%{command}";
+    pub const URL_REPLACE: &str = "%{url}";
+    pub const DOMAIN_REPLACE: &str = "%{domain}";
+    pub const ICON_REPLACE: &str = "%{icon}";
 }
