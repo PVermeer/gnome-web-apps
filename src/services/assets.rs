7a2b38536794371a7a522010b699ac9b68046d72
Pim Vermeer <pim.vermeer@gmail.com>
2025-11-27 19:06:50 +0000

    chore(assets): remove unnecessary dev path overwrite

--- src/services/assets.rs
+++ src/services/assets.rs
@@ -1,44 +1,28 @@
 use crate::services::app_dirs::AppDirs;
 use anyhow::Result;
 use include_dir::{Dir, include_dir};
-use std::{
-    cell::RefCell,
-    fs,
-    path::{Path, PathBuf},
-    rc::Rc,
-};
+use std::{fs, rc::Rc};
 use tracing::info;
 
 static ASSETS: Dir = include_dir!("$CARGO_MANIFEST_DIR/assets");
 
 pub struct Assets {
     app_dirs: Rc<AppDirs>,
-    config_dir: RefCell<PathBuf>,
 }
 impl Assets {
     pub fn new(data_dirs: &Rc<AppDirs>) -> Self {
         Self {
             app_dirs: data_dirs.clone(),
-            config_dir: RefCell::new(PathBuf::new()),
         }
     }
 
     pub fn init(&self) -> Result<()> {
-        let mut config_dir = self.config_dir.borrow_mut();
-        *config_dir = self.app_dirs.config();
-
-        if cfg!(debug_assertions) {
-            *config_dir = Path::new("dev-config").to_path_buf();
-        }
-        drop(config_dir);
-
         self.create_config_files()?;
-
         Ok(())
     }
 
     fn create_config_files(&self) -> Result<()> {
-        let config_dir = &*self.config_dir.borrow();
+        let config_dir = self.app_dirs.config();
 
         // Docs: 'Fails if some files already exist' is not true.
         // It will overwrite existing files.
@@ -49,7 +33,7 @@ impl Assets {
     }
 
     pub fn reset_config_files(&self) -> Result<()> {
-        let config_dir = &*self.config_dir.borrow();
+        let config_dir = self.app_dirs.config();
 
         if config_dir.is_dir() {
             info!("Deleting config files");
